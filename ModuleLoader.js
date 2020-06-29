ffd138c3a3d2b6a6887fd1fe47d1ac05aa0bc954
ChrisMagnuson <hourglasssand@gmail.com>
2020-06-29 08:40:04 -0400

    Initial commit

---
+++ ModuleLoader.js
@@ -0,0 +1,6 @@
+
+//When https://github.com/tc39/proposal-top-level-await is implemented we won't need the loader
+//It exists to emulate the effect of level await for things within the module 
+import Start_Module from './Module.js'
+
+Start_Module()
